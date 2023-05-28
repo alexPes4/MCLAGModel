2cb0f6062f3f025b7f2567372b72db2a87218f35
alexPes4 <92215421+alexPes4@users.noreply.github.com>
2023-05-28 16:58:09 +0300

    Delete src/main directory

--- src/main/java/com/diploma/LAGmodel/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.diploma.LAGmodel;
--- src/main/java/com/diploma/LAGmodel/repository/NetworkElementsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.diploma.LAGmodel.repository;
-
-import com.diploma.LAGmodel.model.ObjectEL;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NetworkElementsRepository extends JpaRepository<ObjectEL, Long> {
-}
